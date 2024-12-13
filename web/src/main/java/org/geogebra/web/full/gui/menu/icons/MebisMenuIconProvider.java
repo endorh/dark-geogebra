339531906fbfb868e7aef53a0baa70bc1010345c
csillasolyom-gecse <csilla@geogebra.at>
2024-12-13 09:53:08 +0100

    MOW-1603 add mebis icons

--- web/src/main/java/org/geogebra/web/full/gui/menu/icons/MebisMenuIconProvider.java
+++ web/src/main/java/org/geogebra/web/full/gui/menu/icons/MebisMenuIconProvider.java
@@ -2,6 +2,7 @@ package org.geogebra.web.full.gui.menu.icons;
 
 import org.geogebra.common.gui.menu.MenuIcon;
 import org.geogebra.web.html5.gui.view.IconSpec;
+import org.geogebra.web.html5.main.toolbox.FaIconSpec;
 
 /**
  * Gives access to Mebis menu icons.
@@ -18,6 +19,28 @@ public class MebisMenuIconProvider extends DefaultMenuIconProvider {
 
 	@Override
 	public IconSpec matchIconWithResource(MenuIcon icon) {
-		return null;
+		switch (icon) {
+		case CLEAR:
+			return new FaIconSpec("fa-file");
+		case SEARCH:
+			return new FaIconSpec("fa-folder-open");
+		case FOLDER:
+			return new FaIconSpec("fa-folder-arrow-up");
+		case SAVE:
+			return new FaIconSpec("fa-floppy-disk");
+		case EXPORT_FILE:
+			return new FaIconSpec("fa-share-nodes");
+		case EXPORT_IMAGE:
+			return new FaIconSpec("fa-images");
+		case DOWNLOAD:
+			return new FaIconSpec("fa-arrow-down-to-line");
+		case PRINT:
+			return new FaIconSpec("fa-print");
+		case SETTINGS:
+			return new FaIconSpec("fa-gear");
+		case INFO:
+			return new FaIconSpec("fa-circle-info");
+		}
+		return super.matchIconWithResource(icon);
 	}
 }
